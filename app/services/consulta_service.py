f8fccd5347a0e848dc3ba8e002e3a181019b552f
rogeriodelphi <rogeriodelphi@gmail.com>
2021-11-08 02:25:40 -0300

    Listando as consultas de todos os pets cadastrados para um determinado dono

--- app/services/consulta_service.py
+++ app/services/consulta_service.py
@@ -8,8 +8,15 @@ def cadastar_consulta(consulta):
                                              exames_prescritos=consulta.exames_prescritos)
     consulta_bd.save()
 
+
+# filtro para retorna apenas o pet que posssue o determinado ID
 def listar_consultas(id):
     consultas = ConsultaPet.objects.filter(pet=id).all()
     return consultas
 
 
+# filtro para retorna as consultas em que o ID do dono do pet é igual
+# aquele ID que está sendo passado
+def listar_consultas_pets(id):
+    consultas = ConsultaPet.objects.filter(pet__dono=id).all().order_by('-data')
+    return consultas
